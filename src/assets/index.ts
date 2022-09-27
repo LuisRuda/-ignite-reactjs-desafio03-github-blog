a4460d7ac4aa20465c4b7b614977b6f01824b9b6
Luis Paulo Ruda <luispauloruda@gmail.com>
2022-09-26 23:53:34 -0300

    feat: fixed url images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,11 @@
-import arrowUpRightFromSquareSolid from 'arrow-up-right-from-square-solid.svg'
-import buildingSolid from 'building-solid.svg'
-import calendarDaySolid from 'calendar-day-solid.svg'
-import chevronLeftSolid from 'chevron-left-solid.svg'
-import commentSolid from 'comment-solid.svg'
-import githubIcon from 'github-icon.svg'
+import arrowUpRightFromSquareSolid from './arrow-up-right-from-square-solid.svg'
+import buildingSolid from './building-solid.svg'
+import calendarDaySolid from './calendar-day-solid.svg'
+import chevronLeftSolid from './chevron-left-solid.svg'
+import commentSolid from './comment-solid.svg'
+import gitHubBlogLogo from './github-blog-logo.svg'
+import gitHubIcon from './github-icon.svg'
+import headerBackground from './header-background.svg'
 
 export {
   arrowUpRightFromSquareSolid,
@@ -11,5 +13,7 @@ export {
   calendarDaySolid,
   chevronLeftSolid,
   commentSolid,
-  githubIcon,
+  gitHubBlogLogo,
+  gitHubIcon,
+  headerBackground,
 }
